18ec0251339f6b00293bc49835cec377bf55b32a
kasireddysrikanth <kasireddysrikanth27@gmail.com>
2020-12-01 11:56:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,61 +9,71 @@ var div2=document.createElement("div")
     document.body.append(div2)
 function display10(cb) { 
         setTimeout(function () {
-            document.getElementById("status").innerHTML="<h2>please wait for 10 seconds<h2>"
+            document.getElementById("status").innerHTML="<h2>please wait for 10 seconds<h2>";
+            cb(display9)
         }, 1000)
     }
     
     function display9(cb) {
         setTimeout(function () {
-            document.getElementById("status").innerHTML="<h2>please wait for 9 seconds<h2>";          
+            document.getElementById("status").innerHTML="<h2>please wait for 9 seconds<h2>";
+            cb(display8)
         }, 1000)
     }
     
     function display8(cb) {
         setTimeout(function () {
-            document.getElementById("status").innerHTML="<h2>please wait for 8 seconds<h2>"
+            document.getElementById("status").innerHTML="<h2>please wait for 8 seconds<h2>";
+            cb(display7)
         }, 1000)
     }
     
     function display7(cb) {
         setTimeout(function () {
-            document.getElementById("status").innerHTML="<h2>please wait for 7 seconds<h2>";
+            document.getElementById("status").innerHTML="<h2>please wait for 7 seconds<h2>";;
+            cb(display6)
         }, 1000)
     }
     
     function display6(cb) {
         setTimeout(function () {
-            document.getElementById("status").innerHTML="<h2>please wait for 6 seconds<h2>";
+            document.getElementById("status").innerHTML="<h2>please wait for 6 seconds<h2>";;
+            cb(display5)
         }, 1000)
     }
     
     function display5(cb) {
         setTimeout(function () {
-            document.getElementById("status").innerHTML="<h2>please wait for 5 seconds<h2>";
+            document.getElementById("status").innerHTML="<h2>please wait for 5 seconds<h2>";;
+            cb(display4)
         }, 1000)
     }
     
     function display4(cb) {
         setTimeout(function () {
             document.getElementById("status").innerHTML="<h2>please wait for 4 seconds<h2>";
+            cb(display3)
         }, 1000)
     }
     
     function display3(cb) {
         setTimeout(function () {
             document.getElementById("status").innerHTML="<h2>please wait for 3 seconds<h2>";
+            cb(display2)
         }, 1000)
     }
     
     function display2(cb) {
         setTimeout(function () {
             document.getElementById("status").innerHTML="<h2>please wait for 2 seconds<h2>";
+            cb(display1)
         }, 1000)
     }
     
     function display1(cb) {
         setTimeout(function () {
             document.getElementById("status").innerHTML="<h2>please wait for 1 second<h2>";
+            cb(message)
         }, 1000)
     }
     
@@ -94,3 +104,7 @@ function display10(cb) {
             })
         })
     })
+
+
+    
+            
